bba8e9922167ef42e13a464c6891722a6af1c41f
Paz Reingold <pazreingold1996@gmail.com>
2017-06-14 11:26:32 +0300

    Initial commit

---
+++ encryptor/src/main/java/encryptor/Encryptor.java
@@ -0,0 +1,12 @@
+package encryptor;
+
+public class Encryptor {
+	public static void main(String[] args){
+		getInput();
+	}
+	
+	public static void getInput(){
+		System.out.println("First");
+	}
+	
+}
---
+++ encryptor/src/test/java/encryptor/getInputTest.java
@@ -0,0 +1,14 @@
+package encryptor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class getInputTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
